476b6dd6f4ab6d7eae828a7222d14aa86d748a0d
fuzikt <38254115+fuzikt@users.noreply.github.com>
2025-08-18 16:30:21 +0200

    set mrcversion to 0 to produce same results (sign) for ori shift as for e2proc3d

--- mrcpy_postprocess_icos_map.py
+++ mrcpy_postprocess_icos_map.py
@@ -5,13 +5,16 @@ import mrcfile
 import numpy as np
 import voltools
 
+
 def main():
     # Create a command-line parser
     parser = argparse.ArgumentParser(description="Post process icosahedral map for model building.")
     parser.add_argument('--i', required=True, help="Input MRC file")
     parser.add_argument('--o', required=True, help="Output prefix")
-    parser.add_argument('--i4to1', action='store_true', help="Rotate the MRC file from I4 icosahedral symmetry to I1 (i222) ")
-    parser.add_argument('--crop', type=int, default=-1, help="Crop the MRC file to a cube of this size in pixels (default: -1, no cropping)")
+    parser.add_argument('--i4to1', action='store_true',
+                        help="Rotate the MRC file from I4 icosahedral symmetry to I1 (i222) ")
+    parser.add_argument('--crop', type=int, default=-1,
+                        help="Crop the MRC file to a cube of this size in pixels (default: -1, no cropping)")
     parser.add_argument('--norm', action='store_true', help="Normalize the MRC file to mean=0 and SD=1")
     parser.add_argument('--flip', action='store_true', help="Flip the MRC file along the x-axis")
     parser.add_argument('--shift_origin', action='store_true', help="Shift origin to the center of the map")
@@ -43,16 +46,15 @@ def main():
     if args.i4to1:
         print("Rotating MRC file from I4 icosahedral symmetry to I1 (i222)...")
         if args.gpu:
-            use_device='gpu'
+            use_device = 'gpu'
         else:
-            use_device='cpu'
+            use_device = 'cpu'
         outData = voltools.transform(outData, interpolation='linear', device=use_device,
-                               rotation=(0, -58.283, 0), rotation_units='deg', rotation_order='rzyz')
-
+                                     rotation=(0, -58.283, 0), rotation_units='deg', rotation_order='rzyz')
 
     if args.crop > 0:
         print(f"Cropping MRC file to {args.crop} pixels...")
-        #make even size
+        # make even size
         crop_size = args.crop
         if crop_size % 2 != 0:
             crop_size += 1
@@ -78,17 +80,21 @@ def main():
         outData = np.roll(outData, 1, axis=2)  # Roll to adjust the origin of symmetry
         output_mrc_prefix += "_flip"
 
+    if args.shift_origin:
+        output_mrc_prefix += "_shift_ori"
+
     # Write the output MRC file
-    with mrcfile.new(output_mrc_prefix+".mrc", overwrite=True) as outMrc:
+    with mrcfile.new(output_mrc_prefix + ".mrc", overwrite=True) as outMrc:
         outMrc.set_data(outData)
         outMrc.update_header_from_data()
         outMrc.update_header_stats()
         outMrc.voxel_size = (voxel_size)
         outMrc.nstart = (nstart)
+        outMrc.header.nversion = 0
         if args.shift_origin:
-            outMrc.header.origin.x = -(outData.shape[2] * voxel_size.x)/2
-            outMrc.header.origin.y = -(outData.shape[1] * voxel_size.y)/2
-            outMrc.header.origin.z = -(outData.shape[0] * voxel_size.z)/2
+            outMrc.header.origin.x = round(-(outData.shape[2] * voxel_size.x) / 2, 1)
+            outMrc.header.origin.y = round(-(outData.shape[1] * voxel_size.y) / 2, 1)
+            outMrc.header.origin.z = round(-(outData.shape[0] * voxel_size.z) / 2, 1)
         else:
             outMrc.header.origin.x = 0.0
             outMrc.header.origin.y = 0.0
@@ -99,17 +105,19 @@ def main():
         outData = np.roll(outData, outData.shape[2] // 2, axis=2)
         outData = np.roll(outData, outData.shape[1] // 2, axis=1)
         outData = np.roll(outData, outData.shape[0] // 2, axis=0)
+        output_mrc_prefix = output_mrc_prefix.replace("_shift_ori", "")
         output_mrc_prefix += "_p23"
-        with mrcfile.new(output_mrc_prefix+".mrc", overwrite=True) as outMrc:
+        with mrcfile.new(output_mrc_prefix + ".mrc", overwrite=True) as outMrc:
             outMrc.set_data(outData)
             outMrc.update_header_from_data()
             outMrc.update_header_stats()
             outMrc.voxel_size = (voxel_size)
             outMrc.nstart = (nstart)
-            outMrc.header.ispg = 195 # Set P23 symmetry
+            outMrc.header.ispg = 195  # Set P23 symmetry
             outMrc.header.origin.x = 0.0
             outMrc.header.origin.y = 0.0
             outMrc.header.origin.z = 0.0
+
 
 if __name__ == "__main__":
     main()
